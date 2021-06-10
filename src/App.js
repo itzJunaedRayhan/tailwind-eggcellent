564abcd33fe72a3930a0f03e1fc22f3733ff83e0
Junaed Rayhan <junaedrayhan@gmail.com>
2021-06-10 23:37:54 +0600

    Navbar Done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 
 import './App.css';
+import Navbar from './Components/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <h1 className="text-red-500">Allah Mohan</h1>
+      <Navbar/>
     </div>
   );
 }
