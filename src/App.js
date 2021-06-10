ce4dcbc5eafe0f2544dbc44c815a8073a5c4f1b0
Junaed Rayhan <junaedrayhan@gmail.com>
2021-06-11 00:47:37 +0600

    Dropdown Menu Done

--- src/App.js
+++ src/App.js
@@ -1,18 +1,47 @@
-
-import './App.css';
-import Content from './Components/Content';
-import Footer from './Components/Footer';
-import Hero from './Components/Hero';
+import React, { useState, useEffect } from 'react';
 import Navbar from './Components/Navbar';
+import { Switch, Route } from 'react-router-dom';
+import Home from './Pages';
+import About from './Pages/about';
+import Menu from './Pages/menu';
+import Footer from './Components/Footer';
+import Dropdown from './Components/Dropdown';
+import Contact from './Pages/contact';
 
 function App() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => {
+    setIsOpen(!isOpen);
+  };
+
+  useEffect(() => {
+    const hideMenu = () => {
+      if (window.innerWidth > 768 && isOpen) {
+        setIsOpen(false);
+        console.log('i resized');
+      }
+    };
+
+    window.addEventListener('resize', hideMenu);
+
+    return () => {
+      window.removeEventListener('resize', hideMenu);
+    };
+  });
+
   return (
-    <div className="App">
-      <Navbar/>
-      <Hero/>
-      <Content/>
-      <Footer/>
-    </div>
+    <>
+      <Navbar toggle={toggle} />
+      <Dropdown isOpen={isOpen} toggle={toggle} />
+      <Switch>
+        <Route path='/' exact component={Home} />
+        <Route path='/menu' component={Menu} />
+        <Route path='/about' component={About} />
+        <Route path='/contact' component={Contact} />
+      </Switch>
+      <Footer />
+    </>
   );
 }
 
---
+++ src/Pages/menu.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Menu = () => {
+  return (
+    <div className='h-screen flex justify-center items-center bg-green-400'>
+      <h1 className='lg:text-9xl md:text-7xl sm:text-5xl text-3xl font-black mb-14'>Menu Page</h1>
+    </div>
+  );
+};
+
+export default Menu;
